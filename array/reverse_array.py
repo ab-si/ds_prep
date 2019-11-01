ca563138537955f693b826d20a3f0ffadb3298e4
abhishek.singh <abhishek.singh@arista.com>
2019-11-01 21:59:19 +0530

    Array questions

---
+++ array/reverse_array.py
@@ -0,0 +1,23 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the reverseArray function below.
+def reverse_slice(ar):
+    return ar[::-1]
+
+def reversed_bltin(ar):
+    return [ele for ele in reversed(ar)]
+
+def reverse_bltin(ar):
+    ar.reverse()
+    return ar
+
+if __name__ == '__main__':
+    arr = list(map(int, input().rstrip().split()))
+    res = reverse_bltin(arr)
+    print(res)
